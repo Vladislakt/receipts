ea7c3f069c6521350167cf5c307daf8f853d05bf
v.stepanichev <vlad1.999vlad@mail.ru>
2024-07-10 21:20:49 +0300

    метод записи ответа, исправления связанные с разными кодировками строк

--- src/main/java/ReceiptsFileWorker.java
+++ src/main/java/ReceiptsFileWorker.java
@@ -1,60 +1,74 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
-
+import static java.nio.charset.StandardCharsets.UTF_8;
 public class ReceiptsFileWorker {
     ArrayList<String> months; //считанные месяцы
     ArrayList<String> services; //считанные услуги
     ArrayList<ArrayList<String>> connection = new ArrayList<>(); // считанные услуги для каждого месяца
-    String text = "";
+    String text;
     /**
      * Считывание файла в заданном заданием формате
      * @param path путь к файлу
      */
-    public void ReadFile(String path) {
+    private void ReadFile(String path) throws IOException {
         HashSet<String> months = new HashSet<>(); //считанные месяцы
         HashSet<String> services = new HashSet<>(); //считанные услуги
         HashSet<String[]> pairs = new HashSet<>(); // считанные пары услуга-месяц
-        try {
-            File file = new File(path);
-            FileReader fr = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fr);
-            String line = reader.readLine();
-            while (line != null) {
-                String[] words = line.split("\\.|_");
-                services.add(words[0]);
-                months.add(words[1]);
-                pairs.add(new String[]{words[0], words[1]});
-                line = reader.readLine();
-            }
-            this.months = new ArrayList<>(months);
-            this.services = new ArrayList<>(services);
-            ArrayList<String[]> pairs_arr = new ArrayList<>(pairs);
-            for(int i= 0;i < months.size();i++) {
-                connection.add(new ArrayList<>());
-            }
-            for(String[] i: pairs_arr) {
-                int pos = this.months.indexOf(i[1]);
-                connection.get(pos).add(i[0]);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        File file = new File(path);
+        FileReader fr = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fr);
+        String line = reader.readLine();
+        while (line != null) {
+            String[] words = line.split("\\.|_");
+            services.add(words[0]);
+            months.add(words[1]);
+            pairs.add(new String[]{words[0], words[1]});
+            line = reader.readLine();
+        }
+        this.months = new ArrayList<>(months);
+        this.services = new ArrayList<>(services);
+        ArrayList<String[]> pairs_arr = new ArrayList<>(pairs);
+        for(int i= 0;i < months.size();i++) {
+            connection.add(new ArrayList<>());
+        }
+        for(String[] i: pairs_arr) {
+            int pos = this.months.indexOf(i[1]);
+            connection.get(pos).add(i[0]);
         }
     }
 
     /**
      * Запись результата работы
      * @param path путь к изначальному файлу
-     * @param text результат, который будет записан в файл
      */
-    public static void WriteFile(String path, String text) {
-
+    public void WriteFile(String path) {
+        try {
+            ReadFile(path);
+            Analyse();
+        }
+        catch (IOException e) {
+            String error_text = "Произошла ошибка чтения файла\n";
+            text = error_text + Arrays.toString(e.getStackTrace());
+        }
+        String new_path = path.replace(".txt", "_по_папкам.txt");
+        File file = new File(new_path);
+        try(FileWriter writer = new FileWriter(file))
+        {
+            writer.write(text);
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 
-    public void Analyse() {
+    /**
+     * Анализ считанной информации и формирование текста ответа
+     */
+    private void Analyse() {
         StringBuilder textPaid = new StringBuilder();
         StringBuilder textNoPaid = new StringBuilder();
         for (String month : months) {
@@ -71,6 +85,6 @@ public class ReceiptsFileWorker {
                 }
             }
         }
-        text = textPaid + "Не оплачено:\n" + textNoPaid;
+        text = new String(textPaid.toString().getBytes(), UTF_8) + "Не оплачено:\n" + new String(textNoPaid.toString().getBytes(), UTF_8);
     }
 }
