8218924eb18819032754ab7a693dd1f8a3690a71
v.stepanichev <vlad1.999vlad@mail.ru>
2024-07-10 15:52:15 +0300

    заготовка класса для чтения/записи

---
+++ src/main/java/ReceiptsFileWorker.java
@@ -0,0 +1,18 @@
+public class ReceiptsFileWorker {
+    /**
+     * Считывание файла в заданном заданием формате
+     * @param path путь к файлу
+     */
+    public static void ReadFile(String path) {
+
+    }
+
+    /**
+     * Запись результата работы
+     * @param path путь к изначальному файлу
+     * @param text результат, который будет записан в файл
+     */
+    public static void WriteFile(String path, String text) {
+
+    }
+}
